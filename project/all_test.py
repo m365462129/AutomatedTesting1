be1a3c94b4d41586f87da4bb94c5a85021addd16
365462129@qq.com <365462129@qq.com>
2019-12-11 17:23:25 +0800

    自动发邮件

--- project/all_test.py
+++ project/all_test.py
@@ -4,6 +4,9 @@ import time
 import HTMLTestRunner
 import login_order ,category
 
+from mail import MailUtils
+
+
 def create_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(login_order.LoginOrderTestCase))
@@ -12,13 +15,17 @@ def create_suite():
 
 
 if __name__ == '__main__':
-    # suite = create_suite()
-    # file_prefix = time.strftime("%Y-%m-%d %H_%M_%S", time.localtime())
-    # fp = open("./"+file_prefix+"_result.html","wb")
-    # runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"报告",description=u"执行情况",verbosity=2)
-    # runner.run(suite)
-    # fp.close()
 
-    suite = create_suite()
-    runner = unittest.TextTestRunner(verbosity=1)   
-    runner.run(suite)
+    isHtml = True
+    if(isHtml):
+        suite = create_suite()
+        # file_prefix = time.strftime("%Y-%m-%d %H_%M_%S", time.localtime())
+        fp = open("./result.html","wb")
+        runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"报告",description=u"执行情况",verbosity=2)
+        runner.run(suite)
+        fp.close()
+        MailUtils.send_test_report()
+    else:
+        suite = create_suite()
+        runner = unittest.TextTestRunner(verbosity=1)   
+        runner.run(suite)
