e3b3c04b92c889a3a2b764c1574f4afdd894a2cc
365462129@qq.com <365462129@qq.com>
2019-12-24 15:26:17 +0800

    no message

---
+++ 00BasisHelp/test.py
@@ -0,0 +1,56 @@
+# -*- coding: UTF-8 -*-
+
+
+#--------------------导入的函数起别名
+# from time import sleep as myselfsleep
+# myselfsleep(2)
+# print("1111111")
+
+
+#--------------------字符串操作
+# print("0：name is " + "GG"+ ", age is " + str(26))
+# print("1：name is %s, age is %d" %("bruce",30))
+# print("2：name is {}, age is {}" .format("AI",30))
+# print("3：name is {1}, age is {0}" .format(22,"MM"))
+# print("4：name is {n}, age is {a}" .format(n="TT",a=25))
+
+
+#--------------------if else
+# if False:
+#     pass
+# elif False:
+#     pass
+# else:
+#     pass
+
+
+#--------------------list
+# lists = [1,2,3,5,"f"]
+# print(lists)
+# print(lists[0])
+# print(lists[4])
+# print("最后一个元素："+ lists[-1])
+# lists.append("u")#末尾添加元素
+# lists.pop(0)#删除列表中的指定的下标元素
+
+
+#--------------------for
+# for i in range(5):
+#     print(str(i))
+
+# for i in range(0,10,3):
+#     print(str(i))
+
+# test_list = {"1","P","KH"}
+# for item in test_list:
+#     print(item)
+
+
+#--------------------字典
+# dicts = {"username":"zhangsan","password":123456}
+# for k,v in dicts.items():
+#     print("key:"+ str(k) + "  value:" + str(v))
+
+# dicts.pop("username")#删除键
+
+
