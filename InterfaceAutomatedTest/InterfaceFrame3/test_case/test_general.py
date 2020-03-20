d3b57a9782eb84678e1bde31392d7eaeb9fa1724
DESKTOP-B74GLFR\36546 <365462129@qq.com>
2020-03-21 00:54:08 +0800

    no message

--- InterfaceAutomatedTest/InterfaceFrame3/test_case/test_general.py
+++ InterfaceAutomatedTest/InterfaceFrame3/test_case/test_general.py
@@ -4,6 +4,7 @@ import urllib.parse
 from parameterized import parameterized
 import paramunittest
 import json
+import ast
 import readExcel
 import a_helper as Helper
 
@@ -20,18 +21,19 @@ class TestMy(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def setParameters(self, case_name, url, method,query):
+    def setParameters(self, case_name, url, method,query,par):
         self.case_name = str(case_name)
         self.url = url
         self.method = str(method).lower()
         self.query = str(query)
+        self.par = str(par)
 
     def test_my_case(self):
         self.start_req(self.url,self.method,self.query)
         pass
 
     def start_req(self,url,method,query):
-        dict_data = self.get_dic()
+        dict_data = self.get_dic()#最终的数据结果是字典类型（dict）
         
         print("\n正在执行的用例名为:" + self.case_name)
         print("请求url=" + self.url)
@@ -42,18 +44,28 @@ class TestMy(unittest.TestCase):
             result = requests.get(url=url, params=dict_data)
         elif method == 'post':
             result = requests.post(url=url, params=dict_data)
-
         print("服务器响应数据：" + result.text)
         self.analysis_results(result)
 
 
     def get_dic(self):
-        #最终的数据结果是字典类型（dict）
-        tmpurl = self.url + "?" + self.query
-        query = urllib.parse.urlparse(tmpurl).query
-        lst_query = urllib.parse.parse_qsl(query)
-        dict_par = dict(lst_query)
+        return self.get_dic0()
+
+    def get_dic0(self):
+        #在excel中配置:{'key':'b53b3ef09b2d4180a2af64ec761760e7','date':'2018-06-06'}，
+        # 这样就行，不要转义符，不要加引号，取到数据后就是{'key':'b53b3ef09b2d4180a2af64ec761760e7','date':'2018-06-06'}
+        #建议用这一种,简单方便安全
+        return ast.literal_eval(self.par)
+
+    def get_dic1(self):
+        #1.在excel中配置:key=b53b3ef09b2d4180a2af64ec761760e7&date=2018-06-08
+        #2.先要拼接成:http://v.juhe.cn/laohuangli/d?key=b53b3ef09b2d4180a2af64ec761760e7&date=2018-06-08
+        tmpurl = "http://v.juhe.cn/laohuangli/d?key=b53b3ef09b2d4180a2af64ec761760e7&date=2018-06-08"
+        query = urllib.parse.urlparse(tmpurl).query#分割后获取key=b53b3ef09b2d4180a2af64ec761760e7&date=2018-06-08
+        lst_query = urllib.parse.parse_qsl(query)#获取列表
+        dict_par = dict(lst_query)#转成字典
         return dict_par
+
 
     def analysis_results(self,result):
         dic_res = json.loads(result.text)
@@ -62,3 +74,4 @@ class TestMy(unittest.TestCase):
         else:
             self.assertEqual(dic_res["error_code"],0,dic_res["reason"])
         pass
+
